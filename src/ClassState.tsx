6012b03fda629edfb8e2981c6ffb44ddef666d8d
Jonathan <many.d.r.o.2001@hotmail.com>
2025-05-29 21:31:01 -0500

    Use of props, state in both class syntax and function syntax

--- src/ClassState.tsx
+++ src/ClassState.tsx
@@ -1,13 +1,26 @@
 import React from "react";
 
-export class ClassState extends React.Component {
+interface ClassStateProps {
+    name: string
+}
+
+export class ClassState extends React.Component<ClassStateProps> {
+    state = {
+        loading: true,
+        error: false
+    }
+
     render() {
+        const { name } = this.props;
+        const { error, loading} = this.state;
         return (
             <div>
-                <h1>Eliminar UseState</h1>
+                <h1>Eliminar {name}</h1>
                 <p>Por favor, escribe el código de seguridad para comprobar que quieres eliminar.</p>
+                {error && (<p>Error: el código es incorrecto</p>)}
+                {loading && (<p>Cargando...</p>)}
                 <input type="text" placeholder="Código de seguridad" />
-                <button>Comprobar</button>
+                <button onClick={() => this.setState({ error: !error})}>Comprobar</button>
             </div>
         )
         
--- src/UseEffect.tsx
+++ src/UseEffect.tsx
@@ -1,10 +1,24 @@
-export function UseState() {
+import { useState } from "react"
+
+type UseStateProps = {
+    name: string
+}
+
+export function UseState({name}: UseStateProps) {
+    const [error, setError] = useState(false);
+    const [loading] = useState(true)
+
     return (
         <div>
-            <h1>Eliminar UseState</h1>
+            <h1>Eliminar {name}</h1>
             <p>Por favor, escribe el código de seguridad para comprobar que quieres eliminar.</p>
+            {loading && (<p>Cargando...</p>)}
+       
+            {error && (
+                <p>Error: el código es incorrecto</p>
+            )}
             <input type="text" placeholder="Código de seguridad" />
-            <button>Comprobar</button>
+            <button onClick={() => setError(!error)}>Comprobar</button>
         </div>
     )
 }
