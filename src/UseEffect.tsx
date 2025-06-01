1b49311b3ca966a6180d9b223e735cba0d747f7f
Jonathan <many.d.r.o.2001@hotmail.com>
2025-06-01 17:55:36 -0500

    Uso de estados compuestos y uso de condicionales para mostrar distintos contenidos

--- src/UseEffect.tsx
+++ src/UseEffect.tsx
@@ -1,34 +1,95 @@
-import { useEffect, useState } from "react"
+import { useEffect, useState } from "react";
 
 type UseStateProps = {
-    name: string
-}
+  name: string;
+};
 
-export function UseState({name}: UseStateProps) {
-    const [error] = useState(false);
-    const [loading, setLoading] = useState(false);
+const SECURITY_CODE = "paradigma";
 
-    useEffect(() => {
-        console.log('Empezando el efecto');
-        if (loading) {
-            setTimeout(() => {
-                setLoading(!loading);
-            }, 3000);
+export function UseState({ name }: UseStateProps) {
+  const initialState = {
+    error: false,
+    loading: false,
+    value: "",
+    authorized: false,
+    deleted: false,
+  };
+  const [state, setState] = useState(initialState);
+
+  const { error, loading, value, authorized, deleted } = state;
+
+  useEffect(() => {
+    console.log("Empezando el efecto");
+    if (loading) {
+      setState((prev) => ({ ...prev, error: false }));
+      setTimeout(() => {
+        if (value !== SECURITY_CODE) {
+          setState((prev) => ({ ...prev, error: true }));
+        } else {
+          setState((prev) => ({ ...prev, authorized: true }));
         }
-        console.log('Terminando el efecto');
-    }, [loading])
+        setState((prev) => ({ ...prev, loading: !prev.loading }));
+      }, 3000);
+    }
+    console.log("Terminando el efecto");
+  }, [loading]);
 
+  if (!authorized && !deleted) {
     return (
-        <div>
-            <h1>Eliminar {name}</h1>
-            <p>Por favor, escribe el código de seguridad para comprobar que quieres eliminar.</p>
-            {loading && (<p>Cargando...</p>)}
-       
-            {error && (
-                <p>Error: el código es incorrecto</p>
-            )}
-            <input type="text" placeholder="Código de seguridad" />
-            <button onClick={() => setLoading(!loading)}>Comprobar</button>
-        </div>
+      <div>
+        <h1>Eliminar {name}</h1>
+        <p>
+          Por favor, escribe el código de seguridad para comprobar que quieres
+          eliminar.
+        </p>
+        {loading && <p>Cargando...</p>}
+
+        {error && !loading && <p>Error: el código es incorrecto</p>}
+        <input
+          type="text"
+          placeholder="Código de seguridad"
+          value={value}
+          onChange={(event) => {
+            setState((prev) => ({ ...prev, value: event.target.value }));
+            console.log(event.target.value);
+          }}
+        />
+        <button
+          onClick={() => {
+            setState((prev) => ({ ...prev, loading: !prev.loading }));
+          }}
+        >
+          Comprobar
+        </button>
+      </div>
+    );
+  } else if (authorized && !deleted) {
+    return (
+      <>
+        <h1>Eliminar UseState</h1>
+        <p>¿Seguro que quieres eliminar UseState?</p>
+        <button
+          onClick={() => {
+            setState((prev) => ({ ...prev, deleted: true }));
+          }}
+        >
+          Sí, eliminar
+        </button>
+        <button
+          onClick={() => {
+            setState((prev) => ({ ...prev, authorized: false, value: "" }));
+          }}
+        >
+          No, volver
+        </button>
+      </>
+    );
+  } else if (authorized && deleted) {
+    return (
+      <>
+        <h1>UseState fue eliminado</h1>
+        <button onClick={() => {setState(initialState)}}>Recuperar UseState</button>
+      </>
     )
+  }
 }
